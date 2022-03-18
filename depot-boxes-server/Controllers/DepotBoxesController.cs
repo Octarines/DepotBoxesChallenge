ea48cc9b51dc28b1bad7c72fe5ce666ed4042551
james-johnston1 <James.Johnston1@sainsburys.co.uk>
2022-03-18 11:29:54 +0000

    New server side project

---
+++ depot-boxes-server/Controllers/DepotBoxesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace depot_boxes_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepotBoxesController : ControllerBase
+    {
+
+        // GET: api/<DepotBoxesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<DepotBoxesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DepotBoxesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DepotBoxesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DepotBoxesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ depot-boxes-server/Models/MoveTask.cs
@@ -0,0 +1,25 @@
+﻿namespace depot_boxes_server.Models
+{
+    public class MoveTask
+    {
+
+        public string Description { get; set; }
+
+        public int id { get; set; }
+
+        //public TaskType {get;set;}
+
+        public int BoxId { get; set; }
+
+        public DateTime CompleteBy { get; set; }
+
+        public int priority { get; set; }
+
+        public bool IsBlocked { get; set; }
+
+        public bool IsComplete { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
